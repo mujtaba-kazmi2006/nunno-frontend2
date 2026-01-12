951599a5efe60dde670bf59e7423312fca3b902c
Your Name <your@email.com>
2026-01-12 04:27:45 -0800

    Add ErrorBoundary to debug crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/App.css'
-import { AuthProvider } from './contexts/AuthContext'
+import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+        <ErrorBoundary>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </ErrorBoundary>
     </React.StrictMode>,
 )
